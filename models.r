34007a3c933038c73625669e1c9eec332c27b1c9
Yang Xu <innerfirexy@gmail.com>
2016-04-07 14:17:37 -0400

    add models for relative sentence position

--- models.r
+++ models.r
@@ -23,3 +23,15 @@ summary(lmer(entc ~ globalID + (1|convID), dt.swbd[globalID >= 10,])) # beta = 5
 
 # entropy vs. global position, Switchboard, global position >= 10
 summary(lmer(ent ~ globalID + (1|convID), dt.swbd[globalID >= 10,])) # beta = 3.357e-03, p < 0.001
+
+
+# fixed effect of relative sentence position in turns
+# for turns of 2 sentences and 3 sentences only
+
+# turn length 2
+summary(lmer(entc ~ localID + (1|convID) + (1|turnID), dt.bnc[turnLen == 2,])) # beta = 7.940e-02, p<0.001
+summary(lmer(entc ~ localID + (1|convID) + (1|turnID), dt.swbd[turnLen == 2,])) # beta = 3.375e-02, p<0.001
+
+# turn length 3
+summary(lmer(entc ~ localID + (1|convID) + (1|turnID), dt.bnc[turnLen == 3,])) # beta = 5.531e-02, p<0.001
+summary(lmer(entc ~ localID + (1|convID) + (1|turnID), dt.swbd[turnLen == 3,])) # beta = 3.457e-02, p<0.001
