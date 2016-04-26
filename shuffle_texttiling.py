028e3d550fba3b25e73fe080db399e94f2c16740
Yang Xu <innerfirexy@gmail.com>
2016-04-26 14:38:42 -0400

    add texttiling_BNC

--- shuffle_texttiling.py
+++ shuffle_texttiling.py
@@ -5,13 +5,17 @@
 import MySQLdb
 import sys
 import re
+import random
+
+from nltk.tokenize.texttiling import TextTilingTokenizer
+
 
 # get db connection
 def db_conn(db_name):
     # db init: ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
     conn = MySQLdb.connect(host = "127.0.0.1",
                     user = "yang",
-                    port = 1234,
+                    port = 3306,
                     passwd = "05012014",
                     db = db_name)
     return conn
@@ -22,6 +26,7 @@ def proc_BNC():
     # is different from the `xmlID` column in entropy_DEM100 of BNC db
     conn = db_conn('bnc')
     cur = conn.cursor()
+
     query = 'select distinct(convIDStr) from entropy'
     cur.execute(query)
     convIDStr = [t[0] for t in cur.fetchall()]
@@ -74,10 +79,79 @@ def proc_BNC():
 def shuffle_BNC():
     conn = db_conn('bnc')
     cur = conn.cursor()
+    # create the table to store the shuffled sentences
+    query = 'create table if not exists entropy_DEM_full_shuffle \
+        (convId int, originalGlobalId int, sentenceId int, tokens longtext, \
+        episodeId int, inEpisodeId int, primary key(convId, sentenceId))'
+    cur.execute(query)
 
-    query = 'select strLower from entropy_DEM_full'
+    # select all convId from the original entropy_DEM_full table
+    query = 'select distinct(convId) from entropy_DEM_full'
+    cur.execute(query)
+    conv_ids = [t[0] for t in cur.fetchall()]
+
+    # for each convId, shuffle the selected (globalId, strLower) sequence,
+    # and insert the shuffled sequence to entropy_DEM_full_shuffle table
+    for i, cid in enumerate(conv_ids):
+        query = 'select globalId, strLower from entropy_DEM_full where convId = %s'
+        cur.execute(query, [cid])
+        sequence = [(t[0], t[1]) for t in cur.fetchall()]
+        random.shuffle(sequence)
+        # insert
+        for j, item in enumerate(sequence):
+            (original_gid, sent) = item
+            query = 'insert into entropy_DEM_full_shuffle values(%s, %s, %s, %s, %s, %s)'
+            cur.execute(query, (cid, original_gid, j+1, sent, None, None))
+        # print progress
+        sys.stdout.write('\r{}/{} convId shuffled.'.format(i+1, len(conv_ids)))
+        sys.stdout.flush()
+    conn.commit()
+
+# conduct texttiling for the shuffled sentences in entropy_DEM_full_shuffle table
+def texttiling_BNC():
+    conn = db_conn('bnc')
+    cur = conn.cursor()
+    # select unique convId
+    query = 'select distinct(convId) from entropy_DEM_full_shuffle'
+    cur.execute(query)
+    conv_ids = [t[0] for t in cur.fetchall()]
+
+    # for each convId, do texttiling, and update the episodeId and inEpisodeId columns
+    tt = TextTilingTokenizer()
+    for i, cid in enumerate(conv_ids):
+        query = 'select tokens from entropy_DEM_full_shuffle where convId = %s'
+        cur.execute(query, [cid])
+        text = '\n\n\n\t'.join([t[0] for t in cur.fetchall()])
+        try:
+            segmented = tt.tokenize(text)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            if str(exc_obj) == 'Input vector needs to be bigger than window size.' or
+                str(exc_obj) == 'No paragraph breaks were found(text too short perhaps?)': # it means the conversation is too short
+                pass
+            else:
+                raise
+        else:
+            sentence_id = 1
+            for j, seg in enumerate(segmented):
+                epi_id = j + 1
+                sents = [s for s in seg.split('\n\n\n\t') if s != '']
+                for k, s in enumerate(sents):
+                    in_epi_id = k + 1
+                    # update
+                    query = 'update entropy_DEM_full_shuffle set episodeId = %s, inEpisodeId = %s \
+                        where convId = %s and sentenceId = %s'
+                    cur.execute(query, (epi_id, in_epi_id, cid, sentence_id))
+                    sentence_id += 1
+            # print progress
+            sys.stdout.write('\r%s/%s updated' % (i+1, len(conv_ids)))
+            sys.stdout.flush()
+    # commit
+    conn.commit()
+
 
 
 
 # main
 if __name__ == '__main__':
+    texttiling_BNC()
